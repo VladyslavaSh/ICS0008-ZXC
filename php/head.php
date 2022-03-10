6d1924d8626b734335c60f04f23e393359e08a52
dmtrub <dmtrub@ttu.ee>
2022-03-10 03:45:41 +0200

    Responsive navbar

--- php/head.php
+++ php/head.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
 <meta name="author" content="ZXC Team">
 <link rel="stylesheet" href="./css/main.css">
+<script src="https://kit.fontawesome.com/63cbc8046e.js" crossorigin="anonymous"></script>
--- php/navbar.php
+++ php/navbar.php
@@ -3,13 +3,15 @@
         <div class="logo">
             <img src="./img/logo.png" alt="logo">
         </div>
-        <div>
-            <ul>
-                <li><a href="./index.php">Home</a></li>
-                <li><a href="./about.php">About</a></li>
-                <li><a href="./catalog.php">Order</a></li>
-                <li><a href="./policy.php">Policy</a></li>
-            </ul>
-        </div>
+        <input type="checkbox" id="check">
+        <label for="check" class="nav_btn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./about.php">About</a></li>
+            <li><a href="./catalog.php">Order</a></li>
+            <li><a href="./policy.php">Policy</a></li>
+        </ul>
     </nav>
 </header>
