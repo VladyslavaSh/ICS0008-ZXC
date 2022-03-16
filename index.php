078ffe0a65f18a3f5100d619824b90e26dd5cc74
dmtrub <dmtrub@ttu.ee>
2022-03-16 13:21:01 +0200

    footer always at the bottom

--- index.php
+++ index.php
@@ -8,42 +8,43 @@
   </head>
   <body>
     <?php include "./php/navbar.php"; ?>
-    <div class="carousel">
-
-      <div class="slide slide_1">
-        <div class="text">
-          <h1>Placeholder h1 text slide 1</h1>
-          <p>Placeholder p text slide 1</p>
+    <div class="main">
+      <div class="carousel">
+        <div class="slide slide_1">
+          <div class="text">
+            <h1>Placeholder h1 text slide 1</h1>
+            <p>Placeholder p text slide 1</p>
+          </div>
         </div>
-      </div>
 
-      <div class="slide slide_2">
-        <div class="text">
-          <h1>Placeholder h1 text slide 2</h1>
-          <p>Placeholder p text slide 2</p>
+        <div class="slide slide_2">
+          <div class="text">
+            <h1>Placeholder h1 text slide 2</h1>
+            <p>Placeholder p text slide 2</p>
+          </div>
         </div>
-      </div>
 
-      <div class="slide slide_3">
-        <div class="text">
-          <h1>Placeholder h1 text slide 3</h1>
-          <p>Placeholder p text slide 3</p>
+        <div class="slide slide_3">
+          <div class="text">
+            <h1>Placeholder h1 text slide 3</h1>
+            <p>Placeholder p text slide 3</p>
+          </div>
         </div>
+
+        <div class="pointer pointer_left">
+          <img src="./img/index/carousel/pointer_left.png" alt="left_pointer">
+        </div>
+
+        <div class="pointer pointer_right">
+          <img src="./img/index/carousel/pointer_right.png" alt="right_pointer">
+        </div>
+
       </div>
 
-      <div class="pointer pointer_left">
-        <img src="./img/index/carousel/pointer_left.png" alt="left_pointer">
+      <div class="benefits">
+        <h1> We are working hard for your comfort!</h1>
+        <img class="fluid" src="./img/index/people_on_scooters.png" alt="people_on_scooters">
       </div>
-
-      <div class="pointer pointer_right">
-        <img src="./img/index/carousel/pointer_right.png" alt="right_pointer">
-      </div>
-
-    </div>
-
-    <div class="benefits">
-      <h1> We are working hard for your comfort!</h1>
-      <img class="fluid" src="./img/index/people_on_scooters.png" alt="people_on_scooters">
     </div>
     <?php include "./php/footer.php"; ?>
   </body>
