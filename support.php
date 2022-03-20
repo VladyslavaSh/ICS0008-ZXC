96cfbb4a7a872997418d57980b09d4ca28dbcb4a
getara <getara@ttu.ee>
2022-03-20 19:53:46 +0300

    Done some work on catalog.php + created support.php

---
+++ support.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include "./php/head.php"; ?>
+    <title>Support - ZXC</title>
+  </head>
+  <body>
+    <?php include "./php/navbar.php"; ?>
+    <div class="main">
+      <p>This is the support page</p>
+    </div>
+    <?php include "./php/footer.php"; ?>
+  </body>
+</html>
