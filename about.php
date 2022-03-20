22f48b21b20b95cd057133adbe0d4e8d883d8a31
Vladyslava Shekula <shekulavlada@gmail.com>
2022-03-20 17:09:15 +0200

    Add content to about.php

--- about.php
+++ about.php
@@ -6,8 +6,27 @@
   </head>
   <body>
     <?php include "./php/navbar.php"; ?>
-    <div class="main">
-      <h1 style="padding-left:20px;">Here we gonna talk why we are awesome</h1>
+    <div class="main" style="padding-left:20px;">
+        <h1>Here we gonna talk why we are awesome</h1>
+        <h2>History of the company:</h2>
+        <p>The idea of the company was born in 2020, when one of its founders visited Tallinn for the first time for the purpose of tourism and decided to rent a bike. However, dissatisfied with the quality of the bikes, the service of the staff and the overpriced prices, he decided to create a more affordable and high-quality service in Tallinn. After a while, having done a great job, he managed to create a bicycle and scooter rental service - “ZXC”.<br><br>
+            The first service point was opened in Tallinn in April 2021. The “ZXC” brand was officially registered in July 2021. Today we have 5 rental offices in Tallinn and many more in other European cities. We intend to further expand and provide our services not only in Europe, but throughout the world.<br><br>
+            We provide both bicycle and scooter rental services. If you want to spend a day in nature with your family, then you can take a bike. If you came to the city for a short time and would like to visit as many sights as possible, then rent a scooter, because it will be much faster to get around the city on it. Everything depends on your needs and desires. We value our customers very much and want to provide them with first-class service, quality equipment and choice.
+        </p>
+        <img class="image_center" src="img/about/bicycle.jpg"  width="600" height="350">
+        <h2>Our mission:</h2>
+        <p>Cycling in Europe – promoting cycling and developing cycling infrastructure in European cities, developing green, eco- and ethno-tourism.<br><br>
+            We do not use harmful substances that can cause any harm to our planet. Our feature is that we use electric scooters. Our scooters are environmentally friendly and do not produce harmful exhaust gases, so you don't have to worry. The main advantage of our scooters lies in the types of engines. Conventional scooters require gasoline to operate, while electric scooters run on electricity.<br><br>
+            We are very concerned about the environment and we donate part of our earnings to the environmental safety fund.
+        </p>
+        <h2>Achieved goals:</h2>
+        <ul  style="list-style-type: disc">
+            <li>We have formed a huge network of scooter and bicycle rental in Tallinn.</li>
+            <li>Introduced European standards of service quality and service.</li>
+            <li>Our bike and scooter fleet is constantly updated.</li>
+            <li>Every client can get instruction before renting a bike or scooter with experienced instructors.</li>
+        </ul>
+        <img class="image_center" src="img/about/bicycle2.jpg" width="600" height="350">
     </div>
     <?php include "./php/footer.php"; ?>
   </body>
