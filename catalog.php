0f6ef050f11dac44a10b0691416d792ea726f110
Georgi <getara@ttu.ee>
2022-04-20 11:01:59 +0300

    Small changes in catalog.php

--- catalog.php
+++ catalog.php
@@ -10,52 +10,49 @@
     <?php include "./php/tpl/navbar.php"; ?>
     <div class="main">
       <div id="filterBar">
-        <h2>filters</h2>
+        <h2>Filters</h2>
         <form id="filterForm" class="" action="catalog.php" method="get">
-          <p>type</p>
+          <div id="officeSelection">
+            <label for="office1">{Office 1}</label>
+            <input type="radio" id="office1" form="filterForm" name="office" value="1">
+            <label for="office2">{Office 2}</label>
+            <input type="radio" id="office2" form="filterForm" name="office" value="2">
+            <label for="office3">{Office 3}</label>
+            <input type="radio" id="office3" form="filterForm" name="office" value="3">
+            <label for="office4">{Office 4}</label>
+            <input type="radio" id="office4" form="filterForm" name="office" value="4">
+            <label for="office5">{Office 5}</label>
+            <input type="radio" id="office5" form="filterForm" name="office" value="5">
+          </div>
+          <p>Type</p>
           <select class="" name="o">
             <option value="all" selected>all</option>
             <option value="bikes">bikes</option>
             <option value="scooters">scooters</option>
           </select>
-          <p>price</p>
-          <input type="range" name="" value="" min="0" max="10"></br>
-          <p>test range 1</p>
-          <input type="range" name="" value="" min="0" max="10"></br>
-          <input type="number" name="" value="">
-          <p>test range 2</p>
-          <input type="range" name="" value="" min="0" max="10"></br>
-          <input type="number" name="" value="">
-          <input type="submit" value="Apply filters">
-          <p>price</p>
+          <p>Price range</p>
           <div id="priceSlider" class="multiRangeSlider">
-            <input type="range" class="inputLeft" min="0" max="100" value="25">
-            <input type="range" class="inputRight" min="0" max="100" value="75">
+            <input type="range" class="inputLeft" min="0" max="100" value="0">
+            <input type="range" class="inputRight" min="0" max="100" value="100">
             <div class="slider">
               <div class="track"></div>
               <div class="range"></div>
               <div class="thumbLeft"></div>
               <div class="thumbRight"></div>
             </div>
-            <input type="number" name="priceMin" value="">
-            <input type="number" name="priceMax" value="">
+            <div class="sliderLabel">
+              <span class="sliderSpan">
+                <span class="inputSpan"><input type="number" name="priceMin" value="">€</span> -
+                <span class="inputSpan"><input type="number" name="priceMax" value="">€</span>
+              </span>
+            </div>
           </div>
+          <input type="button" name="reset" value="reset">
+          <input type="submit" value="Apply filters">
         </form>
         <input type="button" name="subtract" value="<">
       </div>
       <div id="content">
-        <div id="officeSelection">
-          <label for="office1">{Office 1}</label>
-          <input type="radio" id="office1" form="filterForm" name="office" value="1">
-          <label for="office2">{Office 2}</label>
-          <input type="radio" id="office2" form="filterForm" name="office" value="2">
-          <label for="office3">{Office 3}</label>
-          <input type="radio" id="office3" form="filterForm" name="office" value="3">
-          <label for="office4">{Office 4}</label>
-          <input type="radio" id="office4" form="filterForm" name="office" value="4">
-          <label for="office5">{Office 5}</label>
-          <input type="radio" id="office5" form="filterForm" name="office" value="5">
-        </div>
         <div id="searchbar">
           <input type="search" form="filterForm" name="b" value="" placeholder="Your search request goes here" size="50">
           <select class="" form="filterForm" name="o">
