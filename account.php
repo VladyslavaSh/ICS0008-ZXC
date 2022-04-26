88bef7c3022b4adadcd8764e965f74aa0e59b72f
getara <getara@ttu.ee>
2022-04-26 23:51:35 +0300

    Fixed a bug: Changing user profile while not auth

--- account.php
+++ account.php
@@ -1,5 +1,11 @@
 <?php
   session_start();
+
+  if (!isset($_SESSION["email"])) {
+    $_SESSION["redirect"] = $_SERVER['REQUEST_URI'];
+    header("Location: login.php");
+    die();
+  }
 ?>
 
 <!DOCTYPE html>
